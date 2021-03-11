91f9d1bfe40edf2c451ca99dc1f40a054a68874e
Brusensky Vladislav <brusensky.dev@gmail.com>
2021-03-11 16:37:13 +0200

    Add fetch to API GitHub
    Edit fucntion component Search to class component, add fetch to api, and set dynamicaly value getting from response.

--- src/components/card.js
+++ src/components/card.js
@@ -1,19 +1,28 @@
-export default function Card() {
+import './card.css';
+export default function Card(props) {
+    let user = props.user;
     return (
         <div className="card">
             <div className="card__header">
-                <div className="card__title">Name</div>
-                <div className="card__subtitle">Location</div>
+                <div className="card__title">{user.name}</div>
+                <div className="card__subtitle">{user.location}</div>
             </div>
-            <img src="" alt="avatar"/>
+            <img className="card__avatar" src={user.avatar_url} alt="avatar"/>
             <div className="card__content">
                 <ul>
-                    <li>public repos: </li>
+                    <li>Id: {user.id}</li>
+                    <li>Public repos: {user.public_repos}</li>
+                    <li>Date of creation: {user.created_at}</li>
                 </ul>
             </div>
             <div className="card__actions">
-                <a className="a-button" href="https://github.com/Brusensky-pmr" target="_blank">
-                <span>View Profile</span>
+                <a 
+                    className="a-button" 
+                    href={user.html_url} 
+                    target="_blank"
+                    rel="noreferrer"
+                >
+                    <span>View Profile</span>
                 </a>
             </div>
         </div>
--- src/components/search.js
+++ src/components/search.js
@@ -1,16 +1,38 @@
+import React from 'react';
 import Card from './card';
 import './search.css';
 
-export default function Search() {
-    return (
-        <div className="search__block">
-            <div className="search__input">
-                <h1>Start type github user name</h1>
-                <form action="#">
-                    <input className="input" placeholder="Start type username"/>
-                </form>
+export default class Search extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            error: null,
+            isLoaded: false,
+            user: {},
+        }
+    };
+
+    componentDidMount() {
+        fetch(`https://api.github.com/users/facebook`)
+            .then(res => res.json())
+            .then((result) => {
+                this.setState({
+                    isLoaded: true,
+                    user: result,
+                });
+            });
+    }
+    render() {
+        return (
+            <div className="search__block">
+                <div className="search__input">
+                    <h1>Start type github user name</h1>
+                    <form action="#">
+                        <input className="input" placeholder="Start type username"/>
+                    </form>
+                </div>
+                <Card user={this.state.user}/>
             </div>
-            <Card/>
-        </div>
-    );
+        );
+    }
 }
