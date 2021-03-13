f590e329bca8337099fda5e7e781c7f52ef0ea97
Brusensky Vladislav <brusensky.dev@gmail.com>
2021-03-13 19:24:58 +0200

    Minor fix

--- src/components/main.js
+++ src/components/main.js
@@ -1,7 +1,7 @@
 import Search from './search';
 import Card from './card';
 export default function MainComponent(props) {
-    if(props.id) {
+    if(props.user.id) {
         return (
             <>
             <Search setUser={props.setUser}/>
